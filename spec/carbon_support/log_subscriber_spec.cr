466c2d807af625292f7ab0f5ab3b10b9c76fa54a
Benoist Claassen <benoist.claassen@gmail.com>
2015-10-05 20:55:32 +0200

    added log subscriber

---
+++ spec/carbon_support/log_subscriber_spec.cr
@@ -0,0 +1,61 @@
+require "../spec_helper"
+
+module CarbonSupportTest
+  class MyLogSubscriber < CarbonSupport::LogSubscriber
+    getter :event
+
+    def some_event(event)
+      @event = event
+      info event.name
+    end
+
+    def foo(event)
+      debug "debug"
+      info { "info" }
+      warn "warn"
+    end
+
+    def bar(event)
+      info "#{color("cool", :red)}, #{color("isn't it?", :blue, true)}"
+    end
+
+    def puke(event)
+      raise "puke"
+    end
+  end
+
+  describe CarbonSupport::LogSubscriber do
+    it "logs with colors" do
+      IO.pipe do |r, w|
+        CarbonSupport::LogSubscriber.logger = Logger.new(w).tap do |logger|
+          logger.level = Logger::Severity::DEBUG
+          logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+            io <<  message
+          end
+        end
+        log_subscriber = MyLogSubscriber.new
+        log_subscriber.bar(nil)
+
+        r.gets.should eq "\e[31mcool\e[0m, \e[1m\e[34misn't it?\e[0m\n",
+      end
+    end
+
+    it "loggs" do
+      IO.pipe do |r, w|
+        CarbonSupport::LogSubscriber.logger = Logger.new(w).tap do |logger|
+          logger.level = Logger::Severity::DEBUG
+          logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+            io <<  message
+          end
+        end
+        log_subscriber = MyLogSubscriber.new
+        log_subscriber.foo(nil)
+
+        r.gets.should eq "debug\n"
+        r.gets.should eq "info\n"
+        r.gets.should eq "warn\n"
+      end
+    end
+
+  end
+end
---
+++ src/carbon_support/log_subscriber.cr
@@ -0,0 +1,65 @@
+require "./subscriber"
+
+module CarbonSupport
+  class LogSubscriber < Subscriber
+    # Embed in a String to clear all previous ANSI sequences.
+    CLEAR   = "\e[0m"
+    BOLD    = "\e[1m"
+
+    # Colors
+    BLACK   = "\e[30m"
+    RED     = "\e[31m"
+    GREEN   = "\e[32m"
+    YELLOW  = "\e[33m"
+    BLUE    = "\e[34m"
+    MAGENTA = "\e[35m"
+    CYAN    = "\e[36m"
+    WHITE   = "\e[37m"
+
+    def self.logger
+      @@logger ||= Carbon.logger
+    end
+
+    def self.logger=(logger)
+      @@logger = logger
+    end
+
+    def self.log_subscribers
+      subscribers
+    end
+
+    def logger
+      LogSubscriber.logger
+    end
+
+    def start(name, id, payload)
+      super if logger
+    end
+
+    def finish(name, id, payload)
+      super if logger
+    rescue e : Exception
+      logger.error "Could not log #{name.inspect} event. #{e.class}: #{e.message} #{e.backtrace}"
+    end
+
+    {% for level in ["info", "debug", "warn", "error", "fatal", "unknown"] %}
+      protected def {{level.id}}
+        logger.{{level.id}}(yield) if logger
+      end
+
+      protected def {{level.id}}(progname = nil)
+        logger.{{level.id}}(progname) if logger
+      end
+    {% end %}
+
+    # Set color by using a symbol or one of the defined constants. If a third
+    # option is set to +true+, it also adds bold to the string. This is based
+    # on the Highline implementation and will automatically append CLEAR to the
+    # end of the returned String.
+    macro color(text, color, bold=false)
+      color = {{@type}}::{{color.id.upcase}}
+      bold  = {{bold.id}} ? BOLD : ""
+      "#{bold}#{color}{{text.id}}#{CLEAR}"
+    end
+  end
+end
