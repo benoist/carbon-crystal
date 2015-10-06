b9739a8444d1c51663fe1fa5445e8d8e179b9a37
Jan van der Pas <janvanderpas@gmail.com>
2015-10-07 00:08:24 +0200

    Add a basic MockRequest for mocking a HTTP::Request

--- spec/support/mock_request.cr
+++ spec/support/mock_request.cr
@@ -1,5 +1,16 @@
-class MockRequest < HTTP::Request
-  def initialize
+class MockRequest
 
+  def initialize
   end
+
+  def get(path : String, headers = Hash.new : Hash(String, String | Array(String)))
+    http_headers = HTTP::Headers.new
+
+    headers.each do |key, value|
+      http_headers.add(key, value)
+    end
+
+    HTTP::Request.new("GET", path, http_headers)
+  end
+
 end
