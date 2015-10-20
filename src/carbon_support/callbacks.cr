1d4cfc61d2c52d31ae0077ffba88a5786520a7ec
Benoist Claassen <benoist.claassen@gmail.com>
2015-10-20 20:32:45 +0200

    fixed callbacks being called multiple times

--- src/carbon_support/callbacks.cr
+++ src/carbon_support/callbacks.cr
@@ -9,10 +9,17 @@ module CarbonSupport::Callbacks
     def initialize(@method, @time, only, except, @block)
       @only = only || [] of Symbol
       @except = except || [] of Symbol
+
+      raise ArgumentError.new("Cannot both set only and except for callback #{@method}") if @only.any? && @except.any?
     end
 
     def run_callback(method)
-      block.call if only.includes?(method) || !except.includes?(method)
+      if only.any?
+        return block.call if only.includes?(method)
+      end
+      if except.any?
+        return block.call unless except.includes?(method)
+      end
     end
 
     def before?
@@ -26,24 +33,24 @@ module CarbonSupport::Callbacks
 
   macro included
     macro inherited
-      private def callbacks
+      private def load_callbacks
         super
       end
 
-      private def skip_callbacks
+      private def load_skip_callbacks
         super
       end
     end
   end
 
   macro skip_before_action(method)
-    private def skip_callbacks
+    private def load_skip_callbacks
       previous_def << {{method}}
     end
   end
 
   macro before_action(method, only = nil, except = nil)
-    private def callbacks
+    private def load_callbacks
       callbacks = previous_def
       callbacks << CarbonSupport::Callbacks::Callback.new({{method}}, :before, {{only}}, {{except}}, ->() { {{method.id}} })
       callbacks
@@ -51,7 +58,7 @@ module CarbonSupport::Callbacks
   end
 
   macro after_action(method, only = nil, except = nil)
-    private def callbacks
+  private def load_callbacks
       callbacks = previous_def
       callbacks << CarbonSupport::Callbacks::Callback.new({{method}}, :after, {{only}}, {{except}}, ->() { {{method.id}} })
       callbacks
@@ -59,11 +66,19 @@ module CarbonSupport::Callbacks
   end
 
   private def callbacks
-    @callbacks ||= [] of CarbonSupport::Callbacks::Callback
+    @callbacks ||= load_callbacks
   end
 
   private def skip_callbacks
-    @skip_callbacks ||= [] of Symbol
+    @skip_callbacks ||= load_skip_callbacks
+  end
+
+  private def load_callbacks
+    [] of CarbonSupport::Callbacks::Callback
+  end
+
+  private def load_skip_callbacks
+    [] of Symbol
   end
 
   def run_callbacks(method : Symbol)
